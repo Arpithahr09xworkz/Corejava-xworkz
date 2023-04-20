298142c32df3c8fd1f691227d135dfefc446abd9
Arpithahr09xworkz <arpithahr09.xworkz@gmail.com>
2023-04-20 18:42:23 +0530

    adding BasicJavaProgramms files

---
+++ Basic Java Programms/Election.java
@@ -0,0 +1,17 @@
+class Election
+{
+public static  void main(String args[])
+{
+System.out.println("Ten Messages to Election");
+System.out.println("Bad governments are elected by good citizens who vote.");
+System.out.println("Democracy is about voting and it's about a majority vote.");
+System.out.println("Somewhere inside of all of us is the power to changes the world.");
+System.out.println("Every election is determined by the people who show up.");
+System.out.println("The ignorance of one voter in a  democracy impairs the security of all.");
+System.out.println("Elections determine who is in  power."); 
+System.out.println("Go vote if you want change");
+System.out.println("Vote for Hope vote  for Change");
+System.out.println("Vote for the Student for Stugents");
+System.out.println("No ned to sweat,Vote for someone won't regret,vote().");
+}
+} 
---
+++ Basic Java Programms/Perfumes.java
@@ -0,0 +1,14 @@
+class Perfumes
+{
+public static void main(String args[])
+{
+System.out.println("BRANDS OF PERFUMES");
+System.out.println("FOGG");
+System.out.println("YARDLEY");
+System.out.println("EVA");
+System.out.println("SIGNATURE");
+System.out.println("DENVER");
+System.out.println("SOMETHING SOMETHING");
+System.out.println("POISON");
+}
+}
