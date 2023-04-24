ae9cefac167bdf2beef6955fd3cfd111952f5477
Arpithahr09xworkz <arpithahr09.xworkz@gmail.com>
2023-04-24 19:51:02 +0530

    basic java programms

---
+++ Features.java
@@ -0,0 +1,14 @@
+class Features
+{
+public static void main(String args[])
+{
+System.out.println("List out the features of java");
+System.out.println("Object Oriented");
+System.out.println("Platform independt");
+System.out.println("Portable");
+System.out.println("Robust");
+System.out.println("Secure");
+System.out.println("Interpreted");
+System.out.println("Multi-Threaded");
+}
+}
---
+++ Language.java
@@ -0,0 +1,22 @@
+class Language
+{
+public static void main(String args[])
+{
+System.out.println("List of Language");
+System.out.println("java");
+System.out.println("javascript");
+System.out.println("python");
+System.out.println("c++");
+System.out.println("php");
+System.out.println("sql");
+System.out.println("html");
+System.out.println("css");
+System.out.println("web");
+System.out.println(". net");
+System.out.println("c#");
+System.out.println("advanced java");
+System.out.println("cloud computing");
+System.out.println("ruby");
+System.out.println("perl");
+}
+}
